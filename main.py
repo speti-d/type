27f1be1b6238d0d0d611a0aa9a9f4cc68c56ab76
spet-d <solteszpeter98@gmail.com>
2021-11-10 16:29:39 +0100

    Added a timer to display the time remaining from the current exercise

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class typeWindow(QtWidgets.QWidget):
     
     def initailizeUI(self):
         # Place for things like the layoutmanager 
-        self.setGeometry(100, 100, 300, 300)
+        self.setGeometry(100, 100, 800, 400)
         self.setWindowTitle('Taccs Typer')
         self.practiceUI()
 
@@ -21,11 +21,16 @@ class typeWindow(QtWidgets.QWidget):
 
         # QTimer to mesure the 1 minute for the exercise
         self.exercise_timer = QtCore.QTimer()
-        self.exercise_timer.timeout.connect(self.endExercise)
+        self.exercise_timer.timeout.connect(self.updateTimerClock)
+
+        # Timer label
+        self.timer_label = QtWidgets.QLabel(self)
+        self.timer_label.setText("Time left:\t")
 
         # Label for later use
         self.resopnse_label = QtWidgets.QLabel(self)
-        self.resopnse_label.setText("No good")
+        self.resopnse_label.setText("WPM: \t\t")
+        self.resopnse_label.move(150, 230)
 
         # QPushbutton to start the exercise with
         self.start_button = QtWidgets.QPushButton("Start" ,self)
@@ -34,8 +39,9 @@ class typeWindow(QtWidgets.QWidget):
 
         # QTextEdit widget to display 
         self.exerciseLine = QtWidgets.QTextEdit(self)
-        self.exerciseLine.resize(200, 150)
+        self.exerciseLine.resize(600, 150)
         self.exerciseLine.move(50, 50)
+        self.exerciseLine.setFontPointSize(20)
         self.exerciseLine.setReadOnly(True)
 
         # The lineEdit into we actually want to type
@@ -44,8 +50,8 @@ class typeWindow(QtWidgets.QWidget):
     
     def startExercise(self):
         self.start_button.setDisabled(True)
-        # self.exercise_timer.start(60000) # actual timer for general use
-        self.exercise_timer.start(10000) # ten second timer for testing
+        self.timeleft = 10; # the time the exercise is meant to take
+        self.exercise_timer.start(1000) # timer ticking once every second
         print("exercise started")
         exercise_text = ""
         with open("words.txt", 'r') as f1:
@@ -53,6 +59,20 @@ class typeWindow(QtWidgets.QWidget):
             for n in range(50):
                 exercise_text += random.choice(wordlist) + " "
         self.exerciseLine.setText(exercise_text)
+        self.timer_label.setText(f"Time left: {self.timeleft}")
+        self.textInput.clear()
+        self.textInput.setFocus()
+    
+    def updateTimerClock(self):
+        self.timeleft -= 1
+        self.timer_label.setText(f"Time left: {self.timeleft}")
+        if self.timeleft == 0:
+            self.endExercise()
+    
+    def test(self):
+        pass
+        # TODO: Rename this and make it edit the displayed text
+        # in the exercise TextEdit
     
     def endExercise(self):
         print("exercise ended")
@@ -63,7 +83,8 @@ class typeWindow(QtWidgets.QWidget):
         for index in range(min(len(input_list), len(exercise_list))):
             if exercise_list[index] == input_list[index]:
                 good_words += 1
-        print('Good words:', good_words) # Prints to terminal for now
+        self.resopnse_label.setText('WPM: {}'.format(good_words))
+        print('WPM {}'.format(good_words)) # Prints to terminal for now
         self.start_button.setDisabled(False)
         # TODO: Connect this to the response label, and calculate wpm
         
@@ -71,7 +92,8 @@ class typeLineEdit(QtWidgets.QLineEdit):
     def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
         super(typeLineEdit, self).keyPressEvent(event)
         if event.key() == QtCore.Qt.Key_Space:
-            print("Space was pressed")
+            # print("Space was pressed")
+            window.test()
             # TODO: Actually check how correct the input is on each spacebar press
 
 
